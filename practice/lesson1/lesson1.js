87e87b08253658cb766f5e2b6a9e303cf631417b
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-01-13 20:43:49 +0700

    #6 Type Annotations

--- practice/lesson1/lesson1.js
+++ practice/lesson1/lesson1.js
@@ -1,3 +1,7 @@
-const name = 'tuanna'
-// console.log('The length of name is: ' + name.length)
-console.log('The uppercase of name is: ' + name.toUpperCase())
+let count = 1
+count = 'name'
+console.log('>>> check name: ', count)
+
+const arr = ['a', 'b', 'c']
+arr.push(1)
+console.log('>>> view arr', arr)
--- practice/lesson1/lesson1.ts
+++ practice/lesson1/lesson1.ts
@@ -1,3 +1,7 @@
-const name1: string = 'tuanna'
-console.log('The length of name is: ' + name1.length)
-console.log('The uppercase of name is: ' + name1.toUpperCase())
+let count1: number = 1
+// count1 = 'name' // error
+console.log('>>> check name: ', count1)
+
+const arr1: string[] = ['a', 'b', 'c']
+// arr.push(1) // error
+console.log('>>> view arr', arr1)
