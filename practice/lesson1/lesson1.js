00b3e1e18ba12b53def70ab47682417524f4d514
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-01-15 10:02:12 +0700

    #7 Type Inference

--- practice/lesson1/lesson1.js
+++ practice/lesson1/lesson1.js
@@ -1,7 +1,3 @@
 let count = 1
 count = 'name'
-console.log('>>> check name: ', count)
-
-const arr = ['a', 'b', 'c']
-arr.push(1)
-console.log('>>> view arr', arr)
+console.log('>>> check count: ', count)
--- practice/lesson1/lesson1.ts
+++ practice/lesson1/lesson1.ts
@@ -1,7 +1,6 @@
 let count1: number = 1
 // count1 = 'name' // error
-console.log('>>> check name: ', count1)
+console.log('>>> check count: ', count1)
 
-const arr1: string[] = ['a', 'b', 'c']
-// arr.push(1) // error
-console.log('>>> view arr', arr1)
+let test = ['a', 'b', 'c', 2] // type arr is: string | number
+test.push(10)
