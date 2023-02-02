b799097bdd473af8875ffac68839601a1e18e0f2
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-02-02 15:53:33 +0700

    #17 TypeScript Data Type - Never

--- practice/lesson1/lesson1.ts
+++ practice/lesson1/lesson1.ts
@@ -1,7 +1,5 @@
-const sum = (a: number, b: number): number => {
-  return a + b;
-};
-
-const handleLogs = (mess: string): void => { // void: ko muốn trả về giá trị
-  console.log(mess);
-};
+function handleLog(mess: string): never {
+  // never: chắc chắn ko trả về giá trị
+  // never chỉ dung đc ở function cũ
+  throw Error(mess);
+}
