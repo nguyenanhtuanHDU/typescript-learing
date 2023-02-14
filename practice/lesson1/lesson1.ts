2bf45e2c386b51b22c7562f0d39043b61c1906db
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-02-14 09:24:23 +0700

    #23 TypeScript While Loop

--- practice/lesson1/lesson1.ts
+++ practice/lesson1/lesson1.ts
@@ -1,15 +1,5 @@
-type testType = string | number; //Type Aliases
-
-function addNumberOrString(a: testType, b: testType) {
-  // union type
-  if (typeof a === "number" && typeof b === "number") {
-    return a + b;
-  }
-  if (typeof a === "string" && typeof b === "string") {
-    return a.concat(b);
-  }
-  throw new Error("Parameters must be numbers or strings");
+let couter: number = 1;
+while (couter < 8) {
+  console.log(couter);
+  couter++
 }
-
-console.log('>>> check res: ', addNumberOrString('s', 'a'));
-
