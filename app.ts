4d1188931e1ebcf06624df699fe71c2f68674711
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-08-05 15:28:05 +0700

    shortcut class

--- app.ts
+++ app.ts
@@ -1,26 +1,17 @@
 class Student {
-  private name: string;
-  public age: number;
-  readonly gender: boolean;
+  constructor(
+    private name: string,
+    public age: number,
+    readonly gender: boolean
+  ) {}
 
-  constructor(name: string, age: number, gender: boolean) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-  }
-
-  showInfo() {
-    console.log(
-      'name is' + this.name,
-      '\nage is' + this.age,
-      '\ngender is' + (this.gender ? 'male' : 'female')
-    );
+  show() {
+    console.log('name: ', this.name);
+    console.log('age: ', this.age);
+    console.log('gender: ', this.gender);
   }
 }
 
-const tuan = new Student('anh tuan', 22, true);
-tuan.showInfo();
-
-// console.log(tuan.name); -> failed -> name là private nên ko truy cập được
-console.log(tuan.age); // success -> age là public nên truy cập đc
-// tuan.gender = false -> failed ->  gender là readonly nên ko thể gán được
+const tuan = new Student('tuan', 22, true);
+// console.log(tuan);
+tuan.show();
