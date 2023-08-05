aeecdadd36e46fde04ced1f3599099906ddf0dd9
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-08-05 08:24:05 +0700

    public, private & readonly

--- app.ts
+++ app.ts
@@ -1,16 +1,26 @@
 class Student {
   private name: string;
-  private age: number;
+  public age: number;
+  readonly gender: boolean;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, gender: boolean) {
     this.name = name;
     this.age = age;
+    this.gender = gender;
   }
 
   showInfo() {
-    console.log('name is' + this.name, 'age is' + this.age);
+    console.log(
+      'name is' + this.name,
+      '\nage is' + this.age,
+      '\ngender is' + (this.gender ? 'male' : 'female')
+    );
   }
 }
 
-const tuan = new Student('anh tuan', 22);
+const tuan = new Student('anh tuan', 22, true);
 tuan.showInfo();
+
+// console.log(tuan.name); -> failed -> name là private nên ko truy cập được
+console.log(tuan.age); // success -> age là public nên truy cập đc
+// tuan.gender = false -> failed ->  gender là readonly nên ko thể gán được
