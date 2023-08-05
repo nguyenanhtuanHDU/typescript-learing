996ecc2e3e781d685e6e115b92b43987c15abedf
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-08-05 15:57:51 +0700

    getter & setter

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
 class Student {
+  private couter: number = 1;
+
   constructor(
     private name: string,
     public age: number,
@@ -10,8 +12,21 @@ class Student {
     console.log('age: ', this.age);
     console.log('gender: ', this.gender);
   }
+
+  get getNumber() {
+    return this.couter;
+  }
+
+  set setNumber(num: number) {
+    this.couter = num;
+  }
 }
 
 const tuan = new Student('tuan', 22, true);
 // console.log(tuan);
-tuan.show();
+// tuan.show();
+
+console.log(tuan.getNumber);
+// tuan.setNumber(2);
+tuan.setNumber = 2;
+console.log(tuan.getNumber);
